7fa7b2ec47fe2733c5093b168377918127d7f3df
Daniel Fernandes Silva <danielfs.ti@gmail.com>
2024-04-23 01:12:28 -0300

    new publish

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 let iframe = document.querySelector('#spreadsheet');
 let spreadsheetCode = `
 aHR0cHM6Ly9kb2NzLmdvb2dsZS5jb20vc3ByZWFkc2hlZXRzL2QvMXFEZUVSbnpHWTluQndHdWhw
-T3c5bFlSay05S2NUMk9leEYweXkzWE5rTDAvZWRpdD9ybT1taW5pbWFsI2dpZD05NTU3MjYzNTI=`;
+T3c5bFlSay05S2NUMk9leEYweXkzWE5rTDAvZWRpdD9ybT1taW5pbWFsJnVzcD1zaGFyaW5nJndp
+ZGdldD10cnVlJmFtcDtoZWFkZXJzPWZhbHNl`;
 
 iframe.setAttribute('src', atob(spreadsheetCode));
